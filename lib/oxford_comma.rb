49e50efddb0c24e9c817ad8d694ffba1df49122b
Jordon Terry <jt5493@hotmail.com>
2020-01-31 02:47:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 def oxford_comma(array)
   if array.length >= 2
-    array[-1] = "and " + array[-1]
+    array[-1] = " and " + array[-1]
   end
   if array.length > 2
-    return array.join(", ")
+    return array.join(",")
   else
     return array.join
   end
